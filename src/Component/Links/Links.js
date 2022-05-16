94cd020f07f0c073036f89f7e77adc1839f867c2
Yusen Jiang <yusenjia@buffalo.edu>
2022-05-15 22:21:48 -0700

    add style to submit button

--- src/Component/Links/Links.js
+++ src/Component/Links/Links.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "../Links/Links.css"
+import github from "../../assets/github-brands.svg"
+import linkedin from "../../assets/linkedin-brands.svg"
 
 export default function Links(){
     return(
         <div className="links-container">
             <h1>Email:yusenjsx@gmail.com</h1>
-            <a href="https://yusenjia.github.io/quiz-app" target="_blank"><button className="primary-btnm">Resume</button></a>
+            <div className="social-links">
+                <a href="https://yusenjia.github.io/quiz-app" target="_blank"><button className="primary-btnm">Resume</button></a>
+                <a href="https://github.com/yusenjia" target="_blank"><img src={github} alt="" /></a>
+                <a href="https://github.com/yusenjia" target="_blank"><img src={linkedin} alt="" /></a>
+            </div>
         </div>
     )
 }
