d41733713ac9eba72735952866ad742f4f4d6262
Yusen Jiang <yusenjia@buffalo.edu>
2022-06-16 16:50:40 -0700

    update close icon on mobile view

--- src/Component/Links/Links.js
+++ src/Component/Links/Links.js
@@ -6,11 +6,11 @@ import linkedin from "../../assets/linkedin-brands.svg"
 export default function Links(){
     return(
         <div className="links-container">
-            <h1>Email:yusenjsx@gmail.com</h1>
+            <h1 className="email">Email:yusenjsx@gmail.com</h1>
             <div className="social-links">
                 <button className="primary-btnm" onClick={()=>window.open("/personal-website/resume.pdf", "_blank")}>Resume</button>
                 <a href="https://github.com/yusenjia" target="_blank"><img src={github} alt="" /></a>
-                <a href="https://github.com/yusenjia" target="_blank"><img src={linkedin} alt="" /></a>
+                <a href="https://www.linkedin.com/in/yusen-jiang-621602242/" target="_blank"><img src={linkedin} alt="" /></a>
             </div>
         </div>
     )
